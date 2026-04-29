2929cde476b2bab034872f029528d3c9f44e04e4
kigawa <mail@kigawa.net>
2026-04-29 22:57:57 +0900

    新しいハードウェア構成 (20と50) 用のTerraform設定を追加

---
+++ hardware/20/variables.tf
@@ -0,0 +1,41 @@
+variable "host" {
+  type    = string
+  default = "192.168.1.20"
+}
+
+variable "ssh_user" {
+  type    = string
+  default = "kigawa"
+}
+
+
+variable "k8s_endpoint" {
+  type    = string
+  default = "k8s.kigawa.net"
+}
+
+variable "k8s_version" {
+  description = "Kubernetes minor version (e.g. 1.32)"
+  type        = string
+  default     = "1.32"
+}
+
+variable "control_plane_host" {
+  type    = string
+  default = "k8s4"
+}
+
+variable "control_plane_ssh_user" {
+  type    = string
+  default = "kigawa"
+}
+
+variable "control_plane_ssh_key_bitwarden_id" {
+  type    = string
+  default = "0393671f-6ef0-4650-be98-b364013f8644"
+}
+
+variable "sudo_password_bitwarden_id" {
+  type    = string
+  default = "52b44d60-7cab-429f-929a-b4340139b6d8"
+}
