e1b5aa5ae44e0790d10f06b56eb321412ca2f1b1
kigawa <mail@kigawa.net>
2026-06-22 22:13:18 +0900

    `force_path_style` を `use_path_style` に変更し、変数のエクスポート処理を簡略化

--- hardware/run.sh
+++ hardware/run.sh
@@ -8,9 +8,7 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
 module="${1:?Usage: $0 <module> <terraform-args...>}"
 shift
 
-export AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY
-AWS_ACCESS_KEY_ID=$(bws secret get eb5eb0e8-2a4a-4398-a756-b37000d87d64 | jq -r '.value')
-AWS_SECRET_ACCESS_KEY=$(bws secret get c39086cc-e112-40eb-b19f-b37000d89090 | jq -r '.value')
+export AWS_ACCESS_KEY_ID=$(bws secret get eb5eb0e8-2a4a-4398-a756-b37000d87d64 | jq -r '.value')
+export AWS_SECRET_ACCESS_KEY=$(bws secret get c39086cc-e112-40eb-b19f-b37000d89090 | jq -r '.value')
 
 terraform -chdir="$script_dir/$module" "$@"
